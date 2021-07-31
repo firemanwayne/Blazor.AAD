7ad392cdecf4ebbb54565e68d02cd94100c741c6
Wayne Davis <wdavis2002@gmail.com>
2021-07-31 10:51:58 -0500

    Commit

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shared;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Client
@@ -15,17 +16,18 @@ namespace Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient(HttpConstants.ApiClientName, c
-                => c.BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? "http://localhost:7071" : builder.HostEnvironment.BaseAddress));
+            var url = builder.HostEnvironment.IsDevelopment() ? "http://localhost:7071" : builder.HostEnvironment.BaseAddress;            
 
-            builder.Services.AddHttpClient(HttpConstants.ApiClientName, c
-                => c.BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? "http://localhost:7071" : builder.HostEnvironment.BaseAddress))
+            builder.Services.AddHttpClient(HttpConstants.ApiClientName, c => c.BaseAddress = new Uri(url))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(HttpConstants.ApiClientName));
 
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-                options.ProviderOptions.LoginMode = "redirect";
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("api://bf4e3d2e-3af8-467f-84cf-1a45383f753c/api");
             });
 
             builder.Services.AddOptions();
